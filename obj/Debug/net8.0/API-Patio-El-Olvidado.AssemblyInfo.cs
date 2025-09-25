a4be8be957983b564a7f1dff9b0fb8767f118f67
Sebastian Moreno <semoreno@ext.sancorseguros.com>
2025-09-25 18:19:55 -0300

    implementacion de git ignore para no trackear archivos innecesarios

--- obj/Debug/net8.0/API-Patio-El-Olvidado.AssemblyInfo.cs
+++ obj/Debug/net8.0/API-Patio-El-Olvidado.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API-Patio-El-Olvidado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad83643cb9af140d9ee05efa8c92d74768520d65")]
 [assembly: System.Reflection.AssemblyProductAttribute("API-Patio-El-Olvidado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API-Patio-El-Olvidado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
